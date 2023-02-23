b5c47fa4904e4a74a5708181ae7b36d0150ed42b
Aurélien Talbot <52750711+aurelientalbot@users.noreply.github.com>
2023-02-23 09:26:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+/* Form contact */
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting
+
+  // get the form data
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  // TODO: do something with the form data (e.g. send an email)
+
+  // reset the form
+  form.reset();
+
+  // display a confirmation message
+  alert('Thank you for your message!');
+});
